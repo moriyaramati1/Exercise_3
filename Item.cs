f3e3e4886fcef7db1f451c26a5f904ef76df9533
Moriya Ramati <Moriyaramati1@gmail.com>
2023-10-20 13:44:24 +0300

    Change ToList function and id

--- Item.cs
+++ Item.cs
@@ -4,16 +4,16 @@ namespace exc3
 {
     internal class Item
     {
-        private static int ID = 0;
+        private static int ID = 1;
         public int id { get; private set; }
         public string name { get; private set; }
-        public int onShelfNumber { get; private set; }
+        public int onShelfNumber { get; set; }
         public string type { get; private set; }
         public string kashruth { get; private set; }
-        public DateTime expirationDate{ get; private set; }
+        public DateTime expirationDate { get; private set; }
         public double space { get; private set; }
 
-        public Item(string name, int shelfNumber, string type, string kashruth, DateTime expirationDate, double area)
+        public Item(string name, string type, string kashruth, DateTime expirationDate, double area, int shelfNumber = -1)
         {
             this.id = ID;
             ID++;
@@ -24,24 +24,25 @@ namespace exc3
             this.expirationDate = expirationDate;
             this.space = area;
         }
-        
+
         public override string ToString()
         {
+           
             string PropertiesDesctiption = "Item properties: \n" +
-                " Id: {0} \n " +
-                "Item Name: {1} \n " +
-                "On Shelf Number: {2} \n " +
-                "Item Type: {3} \n "+
-                "Kshruth: {4} \n " +
-                "Expiration Date: {5} \n " +
+                " Id: {0} \t " +
+                "Item Name: {1} \t " +
+                "On Shelf Number: {2} \t " +
+                "Item Type: {3} \n " +
+                "Kshruth: {4} \t " +
+                "Expiration Date: {5} \t " +
                 "Area: {6}";
             string PropertiesString = string.Format(PropertiesDesctiption, id, name, onShelfNumber, type, kashruth, expirationDate, space);
-      
+
             return PropertiesString;
         }
 
-      
+
 
     }
-    
+
 }
